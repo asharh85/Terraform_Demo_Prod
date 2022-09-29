958fcd57230c11feeed4e6bb4219e867d4fd9b69
asharh85 <asharh85@gmail.com>
2022-09-29 02:56:31 -0400

    Update networking.tf

--- networking.tf
+++ networking.tf
@@ -1,32 +1,32 @@
-resource "oci_core_vcn" "non-prod-vcn" {
+resource "oci_core_vcn" "prod-vcn" {
   cidr_block     = "172.16.0.0/16"
   compartment_id = var.compartment_id
-  dns_label      = "non-prod"
-  display_name   = "non-prod-vcn"
+  dns_label      = "prod"
+  display_name   = "prod-vcn"
 }
 
-resource "oci_core_internet_gateway" "non-prod-vcn" {
+resource "oci_core_internet_gateway" "prod-vcn" {
   compartment_id = var.compartment_id
-  display_name   = "non-prod Box"
-  vcn_id         = oci_core_vcn.non-prod-vcn.id
+  display_name   = "prod Box"
+  vcn_id         = oci_core_vcn.prod-vcn.id
   enabled        = true
 }
 
-resource "oci_core_route_table" "non-prod-vcn" {
+resource "oci_core_route_table" "prod-vcn" {
   compartment_id = var.compartment_id
-  vcn_id         = oci_core_vcn.non-prod-vcn.id
-  display_name   = "non-prod Box"
+  vcn_id         = oci_core_vcn.prod-vcn.id
+  display_name   = "prod Box"
 
   route_rules {
     destination       = "0.0.0.0/0"
-    network_entity_id = oci_core_internet_gateway.non-prod-vcn.id
+    network_entity_id = oci_core_internet_gateway.prod-vcn.id
   }
 }
 
-resource "oci_core_security_list" "non-prod-vcn-incoming" {
-  display_name   = "Incoming traffic for non-prod Box"
+resource "oci_core_security_list" "prod-vcn-incoming" {
+  display_name   = "Incoming traffic for prod Box"
   compartment_id = var.compartment_id
-  vcn_id         = oci_core_vcn.non-prod-vcn.id
+  vcn_id         = oci_core_vcn.prod-vcn.id
 
   egress_security_rules {
     protocol    = "all"
@@ -69,15 +69,15 @@ resource "oci_core_security_list" "non-prod-vcn-incoming" {
   }
 }
 
-resource "oci_core_subnet" "non-prod-subnet" {
-  cidr_block        = oci_core_vcn.non-prod-vcn.cidr_block
+resource "oci_core_subnet" "prod-subnet" {
+  cidr_block        = oci_core_vcn.prod-vcn.cidr_block
   compartment_id    = var.compartment_id
-  vcn_id            = oci_core_vcn.non-prod-vcn.id
-  route_table_id    = oci_core_route_table.non-prod-vcn.id
+  vcn_id            = oci_core_vcn.prod-vcn.id
+  route_table_id    = oci_core_route_table.prod-vcn.id
 
-  display_name      = "non-prod-subnet"
-  dns_label         = "non-prod"
+  display_name      = "prod-subnet"
+  dns_label         = "prod"
   security_list_ids = [
-      oci_core_security_list.non-prod-vcn-incoming.id
+      oci_core_security_list.prod-vcn-incoming.id
   ]
 }
